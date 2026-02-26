42a8ccfe6b5fb7e304251a47fd1edb37a444ad37
SPACE-MONARCH <montymilanbiswal27@gmail.com>
2026-02-26 15:36:23 +0530

    UC4:CharacterArrayPalindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,18 +2,25 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Word: ");
         String Word = sc.nextLine();
-        String Reversed = "";
-        for (int i = Word.length() - 1; i >= 0 ;  i--) {
-            Reversed += Word.charAt(i);
+        char[] charArray = Word.toLowerCase().toCharArray();
+        boolean isPalindrome = true;
+        int start = 0;
+        int end = charArray.length - 1;
+        while (start < end) {
+            if (charArray[start] != charArray[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (Word.equals(Reversed)) {
-            System.out.println("The word \"" + Word + "\" is a Palindrome.");
+        if (isPalindrome) {
+            System.out.println("The word \"" + Word + "\" is a Palindrome (Using char array).");
         } else {
-            System.out.println("The word \"" + Word + "\" is NOT a Palindrome.");
+            System.out.println("The word \"" + Word + "\" is NOT a Palindrome (Using char array).");
         }
-        System.out.println("UC3 Completed Successfully.");
+        System.out.println("UC4 Completed Successfully.");
         System.out.println();
     }
 }
